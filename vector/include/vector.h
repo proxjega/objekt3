be435670acf6717969e27e88f13a1a8655947a8e
Nikita Jegorov <proxjega@gmail.com>
2025-04-13 02:55:54 +0300

    vectoring

--- vector/include/vector.h
+++ vector/include/vector.h
@@ -1,11 +1,24 @@
 #pragma once
 
+template<typename T>
 class Vector {
 private:
 	int size_;
 	int capacity_;
-	int* data_;
+	T* data_;
+	T* begin, end;
 public:
-	Vector();
-	~Vector();
+	Vector() {
+		size_ = 10;
+		capacity_ = 10;
+		data_ = new T[size_];
+		begin = end = nullptr;
+	}
+	~Vector() {
+		size_ = 0;
+		capacity_ = 0;
+		delete[] data_;
+		delete begin;
+		delete end;
+	}
 };
