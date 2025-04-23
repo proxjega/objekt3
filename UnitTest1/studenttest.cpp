f156cfd2ca955041f25b6b7df19db50a792394fd
Nikita Jegorov <proxjega@gmail.com>
2025-04-23 15:15:55 +0300

    constructor test

--- UnitTest1/studenttest.cpp
+++ UnitTest1/studenttest.cpp
@@ -17,5 +17,21 @@ namespace studenttest
 			Assert::AreEqual(static_cast<int>(st.getMarks().size()), 0);
 			Assert::AreEqual(st.getExamMark(), 1);
 		}
+		TEST_METHOD(ConstructorWith4Params)
+		{
+			Logger::WriteMessage("ConstructorWith4Params");
+			vector<int>pazymiai = { 1,2,3,4,5 };
+			std::wstring name = L"name";
+			std::wstring surname = L"surname";
+			float mean = (1 + 2 + 3 + 4 + 5) / 5.0 * 0.4 + 0.6 * 10;
+			float median = 3 * 0.4 + 0.6 * 10;
+			Student st(L"name", L"surname", pazymiai, 10);
+			Assert::AreEqual(st.getName(), name);
+			Assert::AreEqual(st.getSurname(), surname);
+			Assert::AreEqual(st.getMarks(), pazymiai);
+			Assert::AreEqual(st.getExamMark(), 10);
+			Assert::AreEqual(st.calculateFinalMean(), mean);
+			Assert::AreEqual(st.calculateFinalMedian(), median);
+		}
 	};
 }
